c7d6746417f58cec9af153524b048c038fc144d7
BeardedPlatypus <mwtegelaers@gmail.com>
2020-09-07 16:11:02 +0200

    Add initial Fabulous project

---
+++ Sprightly.WPF/App.fs
@@ -0,0 +1,21 @@
+﻿namespace Sprightly.WPF
+
+open System
+
+open Xamarin.Forms
+open Xamarin.Forms.Platform.WPF
+
+type MainWindow() = 
+    inherit FormsApplicationPage()
+
+module Main = 
+    [<EntryPoint>]
+    [<STAThread>]
+    let main(_args) =
+
+        let app = new System.Windows.Application()
+        Forms.Init()
+        let window = MainWindow() 
+        window.LoadApplication(new Sprightly.App())
+
+        app.Run(window)
---
+++ Sprightly.WPF/Properties/AssemblyInfo.fs
@@ -0,0 +1,24 @@
+﻿namespace Sprightly.WPF
+open System.Reflection
+open System.Runtime.CompilerServices
+
+// the name of the type here needs to match the name inside the ResourceDesigner attribute
+//type Resources = Sprightly.Droid.Resource
+//[<assembly: Android.Runtime.ResourceDesigner("Sprightly.WPF.Resources", IsApplication=true)>]
+
+[<assembly: AssemblyTitle("Sprightly.WPF")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
