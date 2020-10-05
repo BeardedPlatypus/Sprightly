2f2c2e310cb771ad5cb718f34b08fa82ddca5dd1
BeardedPlatypus <mwtegelaers@gmail.com>
2020-10-05 16:16:27 +0200

    AB#201: Add initial Texture.fs

---
+++ Sprightly/Domain/Texture.fs
@@ -0,0 +1,28 @@
+﻿namespace Sprightly.Domain
+
+/// <summary>
+/// <see cref="Texture"/> defines the <see cref="Texture.T"/> and related
+/// functions.
+/// </summary>
+module public Texture = 
+    /// <summary>
+    /// <see cref="T"/> defines the texture type.
+    /// </summary>
+    type public T = {
+        id: int32
+        name: string
+    }
+
+    /// <summary>
+    /// Construct a new <see cref="T"/>
+    /// </summary>
+    /// <param name="id">The id of the texture. </param>
+    /// <param name="name">The human-readable name of this texture. </param>
+    /// <returns>
+    /// A new texture.
+    /// </returns>
+    let construct (id: int32) (name: string) : T =
+        { id = id
+          name = name
+        }
+
