a909055c4a8ebde464bec3f9d1b1c35da79a61ed
BeardedPlatypus <mwtegelaers@gmail.com>
2020-10-12 20:49:56 +0200

    AB#203: Move RecentProject to a separate file in DataAccess

---
+++ Sprightly/DataAccess/RecentProject.fs
@@ -0,0 +1,10 @@
+﻿namespace Sprightly.DataAccess
+    /// <summary>
+    /// <see cref="RecentProject"/> defines a single recent project with a 
+    /// path and a date when it was last opened.
+    /// </summary>
+    type public RecentProject = 
+        { Path : Sprightly.Domain.Path.T 
+          LastOpened : System.DateTime
+        }
+
