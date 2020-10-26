7bd1a24a093b6586782e275b56cfcf48bdef44b7
BeardedPlatypus <mwtegelaers@gmail.com>
2020-10-26 20:46:07 +0100

    AB#205: Replace FilePicker with IFilePicker interface

---
+++ Sprightly/Components/Common/IFilePicker.fs
@@ -0,0 +1,16 @@
+﻿namespace IFilePicker
+
+open Sprightly
+
+/// <summary>
+/// <see cref="IFilePicker"/> defines the interface with which a file
+/// picker interface can be created.
+/// </summary>
+type public IFilePicker = 
+    /// <summary>
+    /// Pick a file through a file dialog and return the result as a path.
+    /// </summary>
+    /// <returns>
+    /// Upon success the path will be returned; otherwise None.
+    /// </returns>
+    abstract Pick : string -> Domain.Path.T Option
