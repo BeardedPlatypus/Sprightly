572b35809956e620690fe41d68400af177e066e9
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-01 14:46:03 +0100

    AB#205: Add JSON wrapping code

---
+++ Sprightly/DataAccess/Json.fs
@@ -0,0 +1,20 @@
+﻿namespace Sprightly.DataAccess
+
+open Sprightly.Domain
+
+module Json =
+    open Newtonsoft.Json
+
+    type public JsonString = string
+
+    let serialize (obj: 'T): JsonString =
+        JsonConvert.SerializeObject obj
+
+    let writeJsonString (path: Path.T) (content: JsonString): Async<unit> =
+        async {
+            do! Async.SwitchToThreadPool ()
+
+            use stream = new System.IO.StreamWriter (path |> Path.toString)
+            return stream.WriteAsync(content) 
+        } |> Async.Ignore
+
