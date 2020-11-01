cda3a4989153e9bc7d36e667611f2f1f750f7ced
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-01 14:52:51 +0100

    AB#205: Move SolutionFileDescription to SolutionFile

--- Sprightly/DataAccess/SolutionFile.fs
+++ Sprightly/DataAccess/SolutionFile.fs
@@ -13,6 +13,27 @@ module SolutionFile =
                      EmbeddedResource="Sprightly, sprightly_project.json">
 
     /// <summary>
+    /// <see cref="T"/> defines a single solution file.
+    /// </summary>
+    type public Description = 
+        { FileName: string
+          DirectoryPath: Path.T
+        }
+
+    /// <summary>
+    /// Initialise a new <see cref="Description"/>.
+    /// </summary>
+    /// <param name="fileName">The solution file name.</param>
+    /// <param name="directoryPath">The path to the parent directory.</param>
+    /// <returns>
+    /// A new <see cref="Description"/>
+    /// </returns>
+    let public init (fileName: string) (directoryPath: Path.T) : Description =
+        { FileName = fileName
+          DirectoryPath = directoryPath
+        }
+
+    /// <summary>
     /// Write the specified <paramref name="solutionFile"/> to the specified path.
     /// </summary>
     /// <param name="solutionFile">The solution file to write to disk.</param>
