c07530aa147f7b866bfaeff98b3063233c7382d2
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-03 17:27:45 +0100

    AB#205: Extend documentation Json.fs

--- Sprightly/DataAccess/Json.fs
+++ Sprightly/DataAccess/Json.fs
@@ -2,15 +2,36 @@
 
 open Sprightly.Domain
 
-module Json =
+/// <summary>
+/// <see cref="Json"/> wraps the methods to serialize, deserialize, read and write
+/// json records.
+/// </summary>
+module public Json =
     open Newtonsoft.Json
 
+    /// <summary>
+    /// <see cref="JsonString"/> defines a json string to use in this module.
+    /// </summary>
     type public JsonString = string
 
-    let serialize (obj: 'T): JsonString =
+    /// <summary>
+    /// Serialize the provided <paramref name="obj"/>.
+    /// </summary>
+    /// <param name="obj">The object to serialize.</param>
+    /// <returns>
+    /// A serialized <see cref="JsonString"/> describing the provided
+    /// <paramref name="obj"/>.
+    /// </returns>
+    let public serialize (obj: 'T) : JsonString =
         JsonConvert.SerializeObject obj
 
-    let writeJsonString (path: Path.T) (content: JsonString): unit =
+    /// <summary>
+    /// Write the specified <paramref name="content"/> to the specified
+    /// <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">The path to write to.</param>
+    /// <param name="content">The json string to write.</param>
+    let public writeJsonString (path: Path.T) (content: JsonString) : unit =
         let parentDirectory = Path.parentDirectory path
         System.IO.Directory.CreateDirectory ( parentDirectory |> Path.toString ) |> ignore
 
