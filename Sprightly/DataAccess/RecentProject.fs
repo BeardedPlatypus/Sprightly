08b908e556e2b85b5045964ee5984bfd09b4b886
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-04 21:52:49 +0100

    AB#205: Add documentation RecentProject

--- Sprightly/DataAccess/RecentProject.fs
+++ Sprightly/DataAccess/RecentProject.fs
@@ -16,6 +16,13 @@
     module public RecentProject =
         let private recentProjectsKey = "recent_projects"
 
+        /// <summary>
+        /// Load the recent projects from the app properties.
+        /// </summary>
+        /// <returns>
+        /// The list of <see cref="RecentProject"/> records if they can
+        /// be retrieved from the app properties.
+        /// </returns>
         let public loadRecentProjects () : (RecentProject list) option =
             let app = Xamarin.Forms.Application.Current
 
@@ -30,18 +37,34 @@
              with _ -> 
                  None
 
+        /// <summary>
+        /// Save the specified <paramref name="recentProjects"/> to the app properties.
+        /// </summary>
+        /// <param name="recentProjects">The recent projects to save.</param>
         let public saveRecentProjects (recentProjects: RecentProject list) : unit =
             let app = Xamarin.Forms.Application.Current
 
             try 
                 let serializedRecentProjects = Json.serialize recentProjects
                 app.Properties.[recentProjectsKey] <- serializedRecentProjects
-                app.SavePropertiesAsync () |> ignore
+                app.SavePropertiesAsync () |> Async.AwaitTask |> ignore
              with _ -> 
                  do ()
             
-
-        let moveProjectToTopOfRecentProjects (recentProject: RecentProject) : unit =
+        /// <summary>
+        /// Update the stored recent project such that the specified 
+        /// <paramref name="recentProject"/> is moved to the top of the list.
+        /// </summary>
+        /// <param name="recentProject">
+        /// The recent project to move to the top of recent project.
+        /// </param>
+        /// <remarks>
+        /// If <paramref name="recentProject"/> exists within the recent 
+        /// projects, it is moved to the top. If it does not exist within
+        /// the recent project it is added as a new element to the top of
+        /// the recent projects.
+        /// </remarks>
+        let public moveProjectToTopOfRecentProjects (recentProject: RecentProject) : unit =
             loadRecentProjects () 
             |> Option.defaultValue [] 
             |> List.filter (fun x -> x <> recentProject)
