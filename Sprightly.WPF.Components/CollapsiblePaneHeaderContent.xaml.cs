c377679b0e8bf60e3c2444b8ca04220e9b92f805
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-09 16:51:39 +0100

    AB#209: Add CollapsiblePaneHeaderRenderer

---
+++ Sprightly.WPF.Components/CollapsiblePaneHeaderContent.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace Sprightly.WPF.Components
+{
+    /// <summary>
+    /// Interaction logic for CollapsiblePaneHeaderContent.xaml
+    /// </summary>
+    public partial class CollapsiblePaneHeaderContent : UserControl, INotifyPropertyChanged
+    {
+        private string _headerText;
+        private string _panelIcon = "\uf0da";
+
+        /// <summary>
+        /// Creates a new <see cref="CollapsiblePaneHeaderContent"/>.
+        /// </summary>
+        public CollapsiblePaneHeaderContent()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the header text.
+        /// </summary>
+        public string HeaderText
+        {
+            get => _headerText;
+            set
+            {
+                if (value == _headerText)
+                {
+                    return;
+                }
+
+                _headerText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the panel icon.
+        /// </summary>
+        public string PanelIcon
+        {
+            get => _panelIcon;
+            set
+            {
+                if (value == _panelIcon)
+                {
+                    return;
+                }
+
+                _panelIcon = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is open.
+        /// </summary>
+        public void SetIsOpen(bool isOpen) =>
+            PanelIcon = isOpen ? "\uf0d7" : "\uf0da";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Sprightly.WPF/Components/Common/CollapsiblePaneHeaderRenderer.fs
@@ -0,0 +1,48 @@
+﻿namespace Sprightly.WPF
+
+open System.ComponentModel
+open Xamarin.Forms.Platform.WPF
+open Sprightly.WPF.Components
+
+open Sprightly.Components.Common
+
+type CollapsiblePaneHeaderRenderer() =
+    inherit ButtonRenderer()
+
+    override this.OnElementChanged(e: ElementChangedEventArgs<Xamarin.Forms.Button>) = 
+        base.OnElementChanged(e)
+
+        match (this.Control, e.NewElement) with 
+        | null, _ -> do ()
+        | _ , null -> do ()
+        | _ ->
+            let pane = (e.NewElement :?> CollapsiblePaneHeader)
+
+            if (not (this.Control.Content :? CollapsiblePaneHeaderContent)) then
+                this.Control.Content <- CollapsiblePaneHeaderContent()
+
+            let content = this.Control.Content :?> CollapsiblePaneHeaderContent
+            content.SetIsOpen(pane.IsOpen)
+            content.HeaderText <- pane.HeaderText
+
+            this.Control.HorizontalContentAlignment <- System.Windows.HorizontalAlignment.Stretch
+
+
+    override this.OnElementPropertyChanged((sender: obj), (e: PropertyChangedEventArgs)) =
+        if (sender :? CollapsiblePaneHeader) && (this.Control.Content :? CollapsiblePaneHeaderContent) then
+            let content = this.Control.Content :?> CollapsiblePaneHeaderContent
+            let paneHeader = sender :?> CollapsiblePaneHeader
+       
+            if (e.PropertyName = "IsOpen") then
+                content.SetIsOpen(paneHeader.IsOpen)
+            else if e.PropertyName = "HeaderText"  then
+                content.HeaderText <- paneHeader.HeaderText
+        else 
+            do ()
+
+
+// Dummy module to ensure this renderer is exported and picked up by Xamarin.Forms
+module Dummy_CollapsiblePaneHeaderRenderer= 
+    [<assembly: ExportRenderer(typeof<Sprightly.Components.Common.CollapsiblePaneHeader>, 
+                               typeof<CollapsiblePaneHeaderRenderer>)>]
+    do ()
