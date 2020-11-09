ca06f851300284e8e4fb93c3699ab7c2c1f64aa4
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-09 16:50:29 +0100

    AB#209: Add CollapsiblePaneHeader

---
+++ Sprightly/Components/Common/CollapsiblePane.fs
@@ -0,0 +1,23 @@
+﻿namespace Sprightly.Components.Common
+
+open Fabulous
+open Fabulous.XamarinForms
+open Xamarin.Forms
+
+module CollapsiblePane =
+    let view (headerText: string) (isOpen: bool) elements (collapseCommand: bool -> unit) =
+        let childrenContainer = 
+            View.StackLayout(orientation = StackOrientation.Vertical,
+                             children = elements)
+                .Padding(Thickness(5.0))
+
+        let header = View.CollapsiblePaneHeader(isOpen = isOpen, 
+                                                headerText = headerText,
+                                                command = fun () -> collapseCommand (not isOpen))
+
+        View.StackLayout(orientation = StackOrientation.Vertical, 
+                         children = [ yield header  
+                                      if isOpen then yield childrenContainer
+                                    ])
+            .Spacing(0.0)
+
