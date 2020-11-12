4ac2f6dac77dd8bf35e7765da64ed84a82c3dec3
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-12 22:55:42 +0100

    AB#210: Add initial MaterialDesign module

---
+++ Sprightly/Components/Common/MaterialDesign.fs
@@ -0,0 +1,18 @@
+﻿namespace Sprightly.Components.Common
+
+open Fabulous
+open Fabulous.XamarinForms
+open Xamarin.Forms
+
+module public MaterialDesign = 
+    module public ElevationColors =
+        let public dp00 = Color.FromHex("#121212")
+        let public dp01 = Color.FromHex("#434343")
+        let public dp02 = Color.FromHex("#4e4e4e")
+        let public dp03 = Color.FromHex("#535353")
+        let public dp04 = Color.FromHex("#575757")
+        let public dp06 = Color.FromHex("#5f5f5f")
+        let public dp08 = Color.FromHex("#636363")
+        let public dp12 = Color.FromHex("#6a6a6a")
+        let public dp16 = Color.FromHex("#6e6e6e")
+        let public dp24 = Color.FromHex("#717171")
