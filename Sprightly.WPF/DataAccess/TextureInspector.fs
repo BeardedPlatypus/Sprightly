38f1e8ea047412614dbfa435928ad6ed3a249e71
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-15 17:16:57 +0100

    AB#202: Implement TextureInspector

--- Sprightly.WPF/DataAccess/TextureInspector.fs
+++ Sprightly.WPF/DataAccess/TextureInspector.fs
@@ -3,13 +3,25 @@
 open Sprightly
 open Sprightly.DataAccess
 
+open Xamarin.Forms
 
+[<Sealed>]
 type public TextureInspector() = 
     interface ITextureInspector with 
         member this.ReadMetaData (path: Domain.Path.T) : Domain.Texture.MetaData option =
             try
-                use stream = System.IO.File.OpenRead(path |> Domain.Path.toString)
-         
-                None
+                let fileInfo = System.IO.FileInfo(path |> Domain.Path.toString)
+                use stream = fileInfo.OpenRead()
+                use image = System.Drawing.Image.FromStream(stream, false, false)
+
+                Some { Width = Domain.Texture.Pixel image.Width
+                       Height = Domain.Texture.Pixel image.Height
+                       DiskSize = Domain.Texture.Size ((float) fileInfo.Length)
+                     }
             with 
             | _ -> None
+
+
+module Dummy_TextureInspector =
+    [<assembly: Dependency(typeof<TextureInspector>)>]
+    do ()
