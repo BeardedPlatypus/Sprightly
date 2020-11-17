eb3d3c492a0f38fd266df4ca7a0a309e85bdf395
BeardedPlatypus <mwtegelaers@gmail.com>
2020-11-17 22:14:28 +0100

    Add documentation CollapsiblePane.fs

--- Sprightly/Components/Common/CollapsiblePane.fs
+++ Sprightly/Components/Common/CollapsiblePane.fs
@@ -4,7 +4,21 @@ open Fabulous
 open Fabulous.XamarinForms
 open Xamarin.Forms
 
+/// <summary>
+/// <see cref="CollapsiblePane"/> module provides the methods related to 
+/// viewing and updating collapsible panes.
+/// </summary>
 module CollapsiblePane =
+    /// <summary>
+    /// View the collapsible pane defined with the provided arguments.
+    /// </summary>
+    /// <param name="headerText">The text displays on the header of the pane </param>
+    /// <param name="isOpen">Whether the pane is opened or collapsed.</param>
+    /// <param name="elements">The view elements displayed in the pane.</param>
+    /// <param name="collapseCommand">The command to open or collapse the pane</param>
+    /// <returns>
+    /// The styled collapsible pane view element.
+    /// </returns>
     let view (headerText: string) (isOpen: bool) elements (collapseCommand: bool -> unit) =
         let childrenContainer = 
             View.StackLayout(orientation = StackOrientation.Vertical,
